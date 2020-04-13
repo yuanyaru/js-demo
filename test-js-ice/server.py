2c44553f7b410a46401eacad149ddc4c6ac15255
yuanyaru <398916756@qq.com>
2020-04-13 15:17:08 +0800

    update

--- test-js-ice/server.py
+++ test-js-ice/server.py
@@ -1,9 +1,11 @@
 import sys, Ice
 import Demo
+
  
 class PrinterI(Demo.Printer):
     def printString(self, s, current=None):
         print s
+ 
  
 with Ice.initialize(sys.argv) as communicator:
     adapter = communicator.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000")
